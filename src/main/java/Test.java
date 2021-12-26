37ca68de5e7197370b0139f92e06af877a43255a
Serp2015 <serpych@yandex.ru>
2021-12-26 23:26:06 +0400

    GiveMeADiamond var1

---
+++ src/main/java/Test.java
@@ -0,0 +1,17 @@
+public class Test extends Exception {
+    public Test() {
+        super();
+    }
+
+    public Test(String message) {
+        super(message);
+    }
+
+    public Test(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public Test(Throwable cause) {
+        super(cause);
+    }
+}
