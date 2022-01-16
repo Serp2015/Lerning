a991fd6c3d4a00ad3926bd8609dc0c3aa80ec8c8
Serp2015 <serpych@yandex.ru>
2022-01-16 20:22:28 +0400

    Lambda var4

--- src/main/java/Lambda.java
+++ src/main/java/Lambda.java
@@ -1,11 +1,40 @@
+import java.util.Optional;
+
 public class Lambda {
     public static void main(String[] args) {
         Calculate calc = (x, y) -> x * y;
         int number = calc.calculate(2, 5);
         System.out.println(number);
+
+        Optional<String> option = Optional.of("hello");
+        System.out.println(option.get());
+        option.ifPresent(x -> System.out.println(x.length()));
     }
 }
 
 interface Calculate {
     int calculate(int x, int y);
+    default void test() {
+        System.out.println("Test");
+    }
+    static void staticMethod() {
+        System.out.println("staticMethod");
+    }
+}
+
+class TestNew implements Calculate {
+
+    @Override
+    public int calculate(int x, int y) {
+        return 0;
+    }
+
+    @Override
+    public void test() {
+        Calculate.super.test();
+    }
+
+    public void staticMethodImpl() {
+        Calculate.staticMethod();
+    }
 }
