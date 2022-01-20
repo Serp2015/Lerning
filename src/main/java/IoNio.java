0d4b1f7a702ee9649f4bae5e70a364c221046a2d
Serp2015 <serpych@yandex.ru>
2022-01-20 20:16:25 +0400

    IO/NIO var2

--- src/main/java/IoNio.java
+++ src/main/java/IoNio.java
@@ -1,11 +1,22 @@
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.nio.channels.FileChannel;
 
 public class IoNio {
     public static void main(String[] args) {
-        fileChannel();
+//        fileChannel();
+//        inputStream();
+    }
+
+    public static void inputStream() {
+        try (InputStream is = new FileInputStream("d:\\Java\\Courses\\IO_NIO.mp4");
+            OutputStream os = new FileOutputStream("d:\\Java\\Courses\\IO_NIO_test.mp4")) {
+            byte[] buffer = new byte[4096];
+            int r;
+            while ((r = is.read(buffer)) != -1) {
+                os.write(buffer, 0, r);
+            }
+        } catch (IOException e) {
+        }
     }
 
     public static void fileChannel() {
@@ -16,7 +27,6 @@ public class IoNio {
         ) {
             in.transferTo(0, in.size(), out);
         } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 }
